4ee91efba98f69a2716463bbcd05d58e9151e5f2
Jens Peters <jpeters@encom.de>
2018-01-22 08:41:21 +0100

    OLED Test

--- hello.js
+++ hello.js
@@ -1,1 +1,30 @@
-console.log('hi')
+const oled = require('oled-js-pi');
+const pngtolcd = require('png-to-lcd');
+
+const opts = {
+  width: 128,
+  height: 64,
+  address: 0x3D
+};
+
+const oled = new oled(opts);
+
+oled.turnOnDisplay();
+
+oled.drawPixel([
+	[128, 1, 1],
+	[128, 32, 1],
+	[128, 16, 1],
+	[64, 16, 1]
+]);
+
+setTimeout( () => {
+
+  oled.clearDisplay();
+
+  pngtolcd('image.png', true, (err, bitmap) => {
+    oled.buffer = bitmap;
+    oled.update();
+  });
+
+}, 20*1000)
